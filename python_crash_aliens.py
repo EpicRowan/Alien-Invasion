408e5fd65cb6ebf80506f4c7ddbc02a65da86ffc
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-09 10:55:57 -0700

    Alien fleet appears on screen

--- python_crash_aliens.py
+++ python_crash_aliens.py
@@ -55,6 +55,8 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			# Move the ship left
 			self.ship.moving_left = True
+		elif event.key == pygame.K_q:
+			sys.exit()
 		elif event.key == pygame.K_SPACE:
 			# Fire bullets on space bar
 			self._fire_bullet()
@@ -88,20 +90,32 @@ class AlienInvasion:
 
 		#Find the number of aliens in a row
 		#Spacing between each alien is = one alien width
-		alien_width = alien.rect.width
+		alien_width, alien_height = alien.rect.size
 		available_space_x = self.settings.screen_width - (2 * alien_width)
 
 		# to find number of aliens that fit, we divide the available space by 2 times width of an alien
 		# // floor division divides and drops remainder
 		number_aliens_x = available_space_x // (2 * alien_width)
 
-		#Create the first row of aliens
-		for alien_number in range(number_aliens_x):
-			# Create an alien and place in the row
-			alien = Alien(self)
-			alien.x = alien_width + 2 * alien_width * alien_number
-			alien.rect.x = alien.x
-			self.aliens.add(alien)
+		# Determine the number of rows of aliens that fit on the screen
+		ship_height = self.ship.rect.height
+		available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+		number_rows = available_space_y // (2 * alien_height)
+
+		#Create the full fleet of aliens
+		for row_number in range(number_rows):
+			for alien_number in range(number_aliens_x):
+				self._create_alien(alien_number, row_number)
+
+	def _create_alien(self, alien_number, row_number):	
+		"""Create an alien and place in the row"""
+
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+		self.aliens.add(alien)
 
 
 	def update_screen(self):
