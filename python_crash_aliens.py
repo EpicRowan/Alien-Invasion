732a09cf20549d811819a45466626bf8e45c7906
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-09 10:09:54 -0700

    Alien successfully appears on the screen

--- python_crash_aliens.py
+++ python_crash_aliens.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet 
+from alien import Alien 
 
 class AlienInvasion:
 	"""Class to manage game assets and behavior"""
@@ -21,6 +22,9 @@ class AlienInvasion:
 		pygame.display.set_caption("Alien Invasion")
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame. sprite.Group()
+
+		self._create_fleet()
 		
 	def run_game(self):
 		""" Start the main loop for the game """
@@ -77,6 +81,12 @@ class AlienInvasion:
 				self.bullets.remove(bullet)
 		# print(len(self.bullets))
 
+	def _create_fleet(self):
+		"""Create the fleet of aliens"""
+		# One alien
+		alien = Alien(self)
+		self.aliens.add(alien)
+
 
 	def update_screen(self):
 		# redraw the screen each pass of the lopp
@@ -85,6 +95,9 @@ class AlienInvasion:
 
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
+
+		self.aliens.draw(self.screen)
+
 		# tells Python to make the most recently drawn screen visible 			
 		pygame.display.flip()
 			
